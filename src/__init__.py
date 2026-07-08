b7e0ab14c3836fcee9e5caedda5bbaf901c96fba
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-08 20:52:50 +0300

    Import multiple engines in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
-
+# src/__init__.py
+from . import db
+from . import demand_engine
+from . import fraud_engine
+from . import matching_engine
+from . import price_engine
+from . import recommendation_engine
