7959f95854fa26859e7b68d8c97753d2467a26eb
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-09 23:18:12 +0300

    Initialize Tabs package for producer pages

---
+++ pages/producer/Tabs/__init__.py
@@ -0,0 +1,5 @@
+"""Tabs package for producer pages"""
+from .dashboard import render_dashboard
+from .inventory import render_inventory
+from .orders import render_orders
+from .ai_insights import render_ai_insights
