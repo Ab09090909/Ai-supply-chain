11b53a9178f8c74496a3dbddac24ac6622142c2d
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-10 00:24:46 +0300

    Update __init__.py

--- pages/producer/Compnents/__init__.py
+++ pages/producer/Compnents/__init__.py
@@ -1,5 +1,1 @@
-"""Components package for producer pages"""
-from .profile import render_profile, render_edit_profile
-from .product_card import render_product_card
 
-__all__ = ['render_profile', 'render_edit_profile', 'render_product_card']
