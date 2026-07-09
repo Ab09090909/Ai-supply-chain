b50063f39b570653b3e473409c099607d8c5ca23
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-09 23:44:01 +0300

    Define __all__ for Components package

--- pages/producer/Compnents/__init__.py
+++ pages/producer/Compnents/__init__.py
@@ -1,3 +1,5 @@
 """Components package for producer pages"""
 from .profile import render_profile, render_edit_profile
 from .product_card import render_product_card
+
+__all__ = ['render_profile', 'render_edit_profile', 'render_product_card']
