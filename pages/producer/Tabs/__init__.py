07c053b72cf5dde36aa490bdc64ce74bdc3ed2f1
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-09 23:43:13 +0300

    Add __all__ to __init__.py for module exports

--- pages/producer/Tabs/__init__.py
+++ pages/producer/Tabs/__init__.py
@@ -3,3 +3,5 @@ from .dashboard import render_dashboard
 from .inventory import render_inventory
 from .orders import render_orders
 from .ai_insights import render_ai_insights
+
+__all__ = ['render_dashboard', 'render_inventory', 'render_orders', 'render_ai_insights']
